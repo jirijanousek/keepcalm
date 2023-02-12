9c65e6c088cebaf57f81f957ad993fc54adce40d
Markéta Andreev Dragoun <marketa.andreevdragoun@puxdesign.cz>
2023-02-12 11:13:39 +0100

    Init project

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require("gulp");
+const bsync = require("browser-sync").create();
+
+gulp.task("serve", () => {
+  bsync.init({
+    server: {
+      baseDir: "./",
+    },
+  });
+
+  gulp.watch("*.html").on("change", bsync.reload);
+  gulp.watch("*.css").on("change", bsync.reload);
+});
+
+gulp.task("default", gulp.task("serve"));
